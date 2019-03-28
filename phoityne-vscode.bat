92f9f9b8796435b117b837ab54595532e1d4cccd
phoityne.hs <phoityne.hs@gmail.com>
2019-03-28 22:35:58 +0900

    release-0.0.23

--- phoityne-vscode.bat
+++ phoityne-vscode.bat
@@ -1,16 +1,23 @@
 @echo off
 
-set where_cmd=where $path:phoityne-vscode.exe
-set pohityne_path=
-for /f "usebackq delims=" %%a in (`%where_cmd%`) do set pohityne_path=%%a
+set where_hda=where $path:haskell-debug-adapter.exe
+set hda_path=
+for /f "usebackq delims=" %%a in (`%where_hda%`) do set hda_path=%%a
 
-if "%pohityne_path%" equ "" (
+set where_pohityne=where $path:phoityne-vscode.exe
+set pohityne_path=
+for /f "usebackq delims=" %%a in (`%where_pohityne%`) do set pohityne_path=%%a
+
+if "%hda_path%" neq "" (
+  echo "starting hda." 1>&2
+  haskell-debug-adapter.exe --hackage-version=0.0.28.0
+) else if "%pohityne_path%" neq "" (
+  echo "starting phoityne." 1>&2
+  phoityne-vscode.exe --hackage-version=0.0.28.0
+) else (
   echo "Content-Length: 199"
   echo.
   echo {"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"phoityne-vscode.exe is not found. Run 'stack install phoityne-vscode', and put it to PATH environment."}
   exit 1
 )
-
-
-phoityne-vscode.exe --hackage-version=0.0.28.0
 
--- phoityne-vscode.sh
+++ phoityne-vscode.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+export HDA_PATH=`which haskell-debug-adapter`
 export PHOITYNE_PATH=`which phoityne-vscode`
 
-if [ "X" = "X$PHOITYNE_PATH" ]; then
+if [ "X" != "X$HDA_PATH" ]; then
+  echo "starting haskell-debug-adapter" >&2
+  haskell-debug-adapter --hackage-version=0.0.28.0
+elif [ "X" != "X$PHOITYNE_PATH" ]; then
+  echo "starting phoitye-vscoder" >&2
+  phoityne-vscode --hackage-version=0.0.28.0
+else
   echo -e "Content-Length: 194\r\n\r"
   echo '{"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"phoityne-vscode is not found. Run `stack install phoityne-vscode`, and put it to PATH environment."}'
 
   exit 1
 fi
-
-phoityne-vscode --hackage-version=0.0.28.0
-
 
