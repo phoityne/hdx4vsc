c74ae1ad7bd869b849787209c22748085fd890ed
phoityne <phoityne.hs@gmail.com>
2022-11-24 18:06:22 +0900

    release 0.0.29

--- phoityne-vscode.bat
+++ phoityne-vscode.bat
@@ -7,9 +7,9 @@ for /f "usebackq delims=" %%a in (`%where_hda%`) do set hda_path=%%a
 if "%hda_path%" neq "" (
   haskell-debug-adapter.exe
 ) else (
-  echo Content-Length: 199
+  echo Content-Length: 181
   echo.
-  echo {"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"haskell-debug-adapter is not found. Does your shell recognize haskell-debug-adapter?"}
+  echo {"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"haskell-debug-adapter is not found. Does your shell recognize haskell-debug-adapter ?"}
   exit 1
 )
 
--- phoityne-vscode.sh
+++ phoityne-vscode.sh
@@ -5,8 +5,8 @@ export HDA_PATH=`which haskell-debug-adapter`
 if [ "X" != "X$HDA_PATH" ]; then
   haskell-debug-adapter
 else
-  echo -e "Content-Length: 194\r\n\r"
-  echo '{"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"haskell-debug-adapter is not found. Does your shell recognize haskell-debug-adapter?"}'
+  echo -e "Content-Length: 181\r\n\r"
+  echo '{"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"haskell-debug-adapter is not found. Does your shell recognize haskell-debug-adapter ?"}'
 
   exit 1
 fi
